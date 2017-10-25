48d85d4288d9745fbe984b8e261c31807d69d2ce
Seremba John <john.seremba@andela.com>
2017-10-25 15:32:10 +0300

    Add color assests, set toggle and app icons, apply app colors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,31 @@
 import { Component } from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   animations: [
-    trigger('slideInOut', [
+    trigger('sideMenuToggle', [
       state('in', style({
-        transform: 'translate3d(100%, 0, 0)'
+        transform: 'translateX(-80%)'
       })),
       state('out', style({
-        transform: 'translate3d(0, 0, 0)'
+        transform: 'translateX(0)'
       })),
-      transition('in => out', animate('400ms ease-in-out')),
-      transition('out => in', animate('400ms ease-in-out'))
-    ]),
+      transition('in <=> out', animate('400ms ease-in-out'))
+    ])
   ]
 })
-export class AppComponent {
-  menuState: String = 'out';
 
+export class AppComponent {
+  public menuState: String = 'out';
   toggleMenu() {
     this.menuState = this.menuState === 'out' ? 'in' : 'out';
   }
