643adf429351e1f2f277860dd69f8af9224573ef
Edwin Kato <edwin.kato@andela.com>
2017-10-27 16:04:41 +0300

    Link analytics component to analytics menu item

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
-import { PageContentComponent } from './../components/page-content/page-content.component';
-import { StoriesComponent } from './../components/stories/stories.component';
 import { PushNotificationsComponent } from './../components/push-notifications/push-notifications.component';
 import { AssignmentsComponent } from './../components/assignments/assignments.component';
+import { PageContentComponent } from '../components/page-content/page-content.component';
+import { StoriesComponent } from '../components/stories/stories.component';
+import { AnalyticsComponent } from '../components/analytics/analytics.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home',  component: PageContentComponent, data: {
+  { path: 'analytics',  component: AnalyticsComponent },
+  {
+    path: 'home',  component: PageContentComponent, data: {
     title: 'Overview'
   } },
   { path: 'stories',  component: StoriesComponent, data: {
--- src/app/components/menu/mock-menus.ts
+++ src/app/components/menu/mock-menus.ts
@@ -5,8 +5,8 @@ const menuIconsPath = './../../assets/images/icons/';
 export const dataMenu: Menu[] = [
     { id: 1, name: 'Overview', icon: `${menuIconsPath}chart_icon.png`, url: ''},
     { id: 2, name: 'Analytics', icon: `${menuIconsPath}eye_icon.png`, url: ''},
-    { id: 3, name: 'Stories', icon: `${menuIconsPath}clock_icon.png`, url: '/stories'},
-    { id: 4, name: 'Assignments', icon: `${menuIconsPath}tag_icon.png`, url: '/assignments'}
+    { id: 4, name: 'Assignments', icon: `${menuIconsPath}tag_icon.png`, url: '/assignments'},
+    { id: 2, name: 'Analytics', icon: `${menuIconsPath}eye_icon.png`, url: 'analytics'}
 ];
 
 export const notificationMenu: Menu[] = [
