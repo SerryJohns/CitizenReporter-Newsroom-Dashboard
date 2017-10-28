7c1f190bfadd44c3402d512cbdc7a5c628babc21
Seremba John <john.seremba@andela.com>
2017-10-28 11:56:27 +0300

    Get stories as observables

--- src/app/components/stories/stories.component.ts
+++ src/app/components/stories/stories.component.ts
@@ -18,8 +18,19 @@ export class StoriesComponent implements OnInit {
   }
 
   loadStories(): void {
-    this.stories = this.storiesService.getStories();
-    console.log(this.stories);
+    // this.storiesService.getStories().then(function(story){
+    //   console.log(story);
+    // });
+    // this.storiesService.getStories().subscribe(
+    //   x => console.log(x),
+    //   e => console.log(e),
+    //   () => console.log('complete')
+    // );
+
+    this.storiesService.getStories()
+    .forEach(v => console.log(v))
+    .then(() => console.log('complete'))
+    .catch(e => console.log(e));
   }
 
 }
--- src/app/services/stories/get-stories.service.ts
+++ src/app/services/stories/get-stories.service.ts
@@ -1,48 +1,31 @@
 import { Injectable } from '@angular/core';
 import { Parse } from 'parse';
+import { Observable } from 'rxjs/Observable';
 
 import { Story } from './../../components/stories/story.model';
 
 @Injectable()
 export class GetStoriesService {
-
-  getStories(): Story[] {
-    // tslint:disable-next-line:prefer-const
-    let storyList: Story[];
-    let ss: Story;
+  getStories(): Observable<Story> {
     const storiesObj: any = Parse.Object.extend('Story');
     const query: any = new Parse.Query(storiesObj);
-    query.find({
-      success: function(stories) {
-        stories.forEach(story => {
-          ss = <Story>({
-            id: story.id,
-            uploaded: story.get('uploaded'),
-            media: story.get('media'), // TODO assign array
-            localID: story.get('localID'),
-            summary: story.get('summary'),
-            author: story.get('author'),
-            assignment: story.get('assignment'),
-            updatedAt: story.get('updatedAt'),
-            cachedLocation: story.get('cachedLocation'),
-            title: story.get('title'),
-            when: <Date>story.get('when'),
-            createdAt: <Date>story.get('createdAt'),
-            sourceApp: story.get('source_app'),
-            who: story.get('who')
+    return Observable.create(
+      observer => {
+        const toStory = (anyStory: any) => this.toStory(anyStory);
+        query.find().then(function(stories) {
+          stories.forEach(item => {
+            observer.next(toStory(item));
           });
-          this.storyList.push(ss);
+          observer.complete();
+        }, function(error) {
+          observer.error(`Error: ${error}`);
         });
-      },
-      error: function(object, error) {
-        console.log(`error: ${ error }`);
       }
-    });
-    return storyList;
+    );
   }
 
-  toStory(story): Story {
-    let myStory: Story = <Story>({
+  toStory(story: any): Story {
+    return <Story>({
       id: story.id,
       uploaded: story.get('uploaded'),
       media: story.get('media'), // TODO assign array
@@ -58,7 +41,5 @@ export class GetStoriesService {
       sourceApp: story.get('source_app'),
       who: story.get('who')
     });
-    return myStory;
   }
-
 }
