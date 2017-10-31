7790ebbde3e313475c45e72323198456840f4f3c
Seremba John <john.seremba@andela.com>
2017-10-31 17:14:08 +0300

    Initialize push notifications component

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 
 import { PageContentComponent } from './../components/page-content/page-content.component';
 import { StoriesComponent } from './../components/stories/stories.component';
+import { PushNotificationsComponent } from './../components/push-notifications/push-notifications.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
