c413b248a19dc8495d8a582418f133811a96285a
Seremba John <john.seremba@andela.com>
2017-11-01 12:25:07 +0300

    Get app time zones

--- src/app/components/push-notifications/push-notifications.component.ts
+++ src/app/components/push-notifications/push-notifications.component.ts
@@ -1,31 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import { SendPushNotificationService } from './../../services/notifications/send-push-notification.service';
+import { GetLocationsService } from './../../services/notifications/get-locations.service';
 import { Audience } from './audience.model';
 
 @Component({
   selector: 'app-push-notifications',
   templateUrl: './push-notifications.component.html',
   styleUrls: ['./push-notifications.component.css'],
-  providers: [ SendPushNotificationService ]
+  providers: [ SendPushNotificationService, GetLocationsService ]
 })
 export class PushNotificationsComponent implements OnInit {
 
-  constructor(private sendPushNotificationsService: SendPushNotificationService ) { }
+  constructor(
+    private sendPushNotificationsService: SendPushNotificationService,
+    private getLocationsService: GetLocationsService
+  ) { }
 
   today: Date = new Date();
+  locations: String[] = [];
   message: String;
   audience: String;
   location: String;
-  result: boolean;
   success: boolean;
   msg: String;
 
   ngOnInit() {
     this.success = false;
+    this.getLocationsService.getLocations().subscribe(
+      (location) => this.locations.push(location)
+    );
+  }
+
+  closeAlert(): void {
+    this.msg = null;
   }
 
   sendPushNotification(): void {
     let audience: Audience;
+    if (!this.msg) {
+      this.msg = 'Please provide a push notification message!';
+      this.success = false;
+      return;
+    }
+
     switch (this.audience) {
       case 'all':
         audience = <Audience>({
@@ -45,16 +62,15 @@ export class PushNotificationsComponent implements OnInit {
         });
         break;
     }
-    console.log(audience);
-    // this.sendPushNotificationsService.sendPushNotification(this.message, audience)
-    // .then((result) => {
-    //   this.msg = 'Push notification sent successfully';
-    //   this.success = true;
-    // })
-    // .catch((err) => {
-    //   this.msg = err;
-    //   this.success = false;
-    // });
+
+    this.sendPushNotificationsService.sendPushNotification(this.message, audience)
+    .then((result) => {
+      this.msg = 'Push notification sent successfully';
+      this.success = true;
+    }).catch((err) => {
+      this.msg = err;
+      this.success = false;
+    });
   }
 
 }
