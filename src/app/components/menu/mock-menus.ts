f18499e4a7c053b255a400b3695f20375f228a73
Seremba John <john.seremba@andela.com>
2017-11-02 10:15:00 +0300

    Add assignments to the mock menus

--- src/app/components/menu/mock-menus.ts
+++ src/app/components/menu/mock-menus.ts
@@ -6,7 +6,7 @@ export const dataMenu: Menu[] = [
     { id: 1, name: 'Overview', icon: `${menuIconsPath}chart_icon.png`, url: ''},
     { id: 2, name: 'Analytics', icon: `${menuIconsPath}eye_icon.png`, url: ''},
     { id: 3, name: 'Stories', icon: `${menuIconsPath}clock_icon.png`, url: '/stories'},
-    { id: 4, name: 'Assignments', icon: `${menuIconsPath}tag_icon.png`, url: ''}
+    { id: 4, name: 'Assignments', icon: `${menuIconsPath}tag_icon.png`, url: '/assignments'}
 ];
 
 export const notificationMenu: Menu[] = [
