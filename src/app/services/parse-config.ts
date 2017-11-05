48cf2c80f59f62bc001d4d18a4c5820cc7c44c85
Seremba John <john.seremba@andela.com>
2017-11-05 20:42:10 +0300

    Connect parse via https

--- src/app/services/parse-config.ts
+++ src/app/services/parse-config.ts
@@ -2,6 +2,6 @@ import { Parse } from 'parse';
 
 Parse.initialize('11235813');
 Parse.masterKey = '11235813';
-Parse.serverURL = 'http://creporter-server.herokuapp.com/parse';
+Parse.serverURL = 'https://creporter-server.herokuapp.com/parse';
 
 export class ParseConfig { }
