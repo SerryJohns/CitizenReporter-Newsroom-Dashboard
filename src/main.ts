3b85c1cd044f1b7fec91a5bd68d12045906ddefb
Seremba John <john.seremba@andela.com>
2017-11-05 22:21:45 +0300

    Remove dot env conf from main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import { dotenv } from 'dotenv';
-
 if (environment.production) {
   enableProdMode();
-} else {
-  dotenv.load();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
