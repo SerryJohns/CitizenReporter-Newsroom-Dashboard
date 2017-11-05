0d7cc997f4eebae13da8cdc3c323a12532147734
Seremba John <john.seremba@andela.com>
2017-11-05 22:52:46 +0300

    Add prod env variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  APP_ID: 11235813,
+  MASTER_KEY: 11235813,
+  SERVER_URL:  'https://creporter-server.herokuapp.com/parse'
 };
