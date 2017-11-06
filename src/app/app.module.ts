10666f4a9936a207b7e51ee44b34b38e218a2d87
Edwin Kato <edwin.kato@andela.com>
2017-11-06 11:09:29 +0300

    Add login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpModule } from '@angular/http';
-import {NgxChartsModule} from '@swimlane/ngx-charts';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './components/app/app.component';
 import { MenuComponent } from './components/menu/menu.component';
@@ -23,6 +23,7 @@ import { PieChartComponent } from './components/pie-chart/pie-chart.component';
 import { LineChartComponent } from './components/line-chart/line-chart.component';
 import { EventsAnalyticsComponent } from './components/events-analytics/events-analytics.component';
 import { CircularSpinnerComponent } from './components/circular-spinner/circular-spinner.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -43,6 +44,7 @@ import { CircularSpinnerComponent } from './components/circular-spinner/circular
     LineChartComponent,
     EventsAnalyticsComponent,
     CircularSpinnerComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
