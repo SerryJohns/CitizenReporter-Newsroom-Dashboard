2be80d252e145ac612332664cac85a07bdabed28
Edwin Kato <edwin.kato@andela.com>
2017-11-06 10:23:08 +0300

    Rectify analytics events url

--- src/app/services/events-analytics/events-analytics.service.ts
+++ src/app/services/events-analytics/events-analytics.service.ts
@@ -4,7 +4,7 @@ import {
 } from '@angular/http';
 import 'rxjs/add/operator/map';
 
-import { getWeekDateRange, getMonthDateRange, makeGetHttpRequest } from '../../utils/utils';
+import { getWeekDateRange, getMonthDateRange, makeGetHttpRequest, formatDate } from '../../utils/utils';
 
 @Injectable()
 export class EventsAnalyticsService {
@@ -17,12 +17,16 @@ export class EventsAnalyticsService {
     'appEvent/day/app/event?metrics=activeDevices,newDevices,timeSpent,' +
     'averageTimePerDevice,averageTimePerSession,' +
     'occurrences&dateTime=' + getMonthDateRange();
+  public analyticsEventsUrl = 'https://api-metrics.flurry.com/public/v1/data/' +
+    'appEvent/all/app/event?metrics=activeDevices,newDevices,timeSpent,' +
+    'averageTimePerDevice,averageTimePerSession,' +
+    'occurrences&dateTime=2017-10-01/' + formatDate(new Date());
 
   constructor(private http: Http) {
   }
 
   public getEventsSummary() {
-    return makeGetHttpRequest(this.http, this.weeklyEventsUrl);
+    return makeGetHttpRequest(this.http, this.analyticsEventsUrl);
   }
 
   public getWeeklyEvents() {
