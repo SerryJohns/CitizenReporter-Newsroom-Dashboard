7f52bc02a4e2b2719b82c3af0e295ecc6eec44d5
Seremba John <john.seremba@andela.com>
2017-12-08 11:43:14 +0300

    Fix env variables

--- dashboard/src/environments/environment.ts
+++ dashboard/src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   production: false,
   APP_ID: '4WcYqncsbH',
   MASTER_KEY: 'aQUDcnUysD',
-  SERVER_URL: 'http://creporter-mfa.dokku.me:1337/parse',
+  SERVER_URL: 'http://creporter-mfa.creporter.codeforafrica.org/parse',
   FLURRY_APP_ANALYTICS_TOKEN:  'eyJhbGciOiJIUzI1NiIsImtpZCI6ImZsdXJyeS56dXVs' +
   'LnByb2Qua2V5c3RvcmUua2V5LjIifQ.eyJpc3MiOiJodHRwczovL3p1dWwuZmx1cnJ5LmNvb' +
   'To0NDMvdG9rZW4iLCJpYXQiOjE1MTE5NjA1NDQsImV4cCI6MzMwNjg4NjkzNDQsInN1YiI6Ij' +
